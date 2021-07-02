d19ca93248886543f634d93ad422f1f12ff3565b
georginarojas <ginarojas29@gmail.com>
2021-07-02 17:10:05 -0300

    Home page and addImage component, completed.

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { Box, Button, Stack, toast, useToast } from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
@@ -19,42 +19,94 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   const formValidations = {
     image: {
       // TODO REQUIRED, LESS THAN 10 MB AND ACCEPTED FORMATS VALIDATIONS
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: ImgFile =>
+          ImgFile[0].size / 1024 / 1024 < 10 ||
+          'O arquivo deve ser menor que 10MB',
+        acceptedFormats: ImgFile =>
+          ImgFile[0].type.match('image/(jpeg|png|gif)') !== null ||
+          'Somente são aceitos arquivos PNG, GIF e JPEG',
+      },
     },
     title: {
       // TODO REQUIRED, MIN AND MAX LENGTH VALIDATIONS
+      required: 'Titulo obrigatório',
+      minLength: {
+        value: 2,
+        message: 'Mínimo de 2 caracteres',
+      },
+      maxLength: {
+        value: 20,
+        message: 'Máximo de 20 caracteres',
+      },
     },
     description: {
       // TODO REQUIRED, MAX LENGTH VALIDATIONS
+      required: 'Descrição obrigatório',
+      maxLength: {
+        value: 65,
+        message: 'Máximo de 65 caracteres',
+      },
     },
   };
 
   const queryClient = useQueryClient();
   const mutation = useMutation(
     // TODO MUTATION API POST REQUEST,
+    async (data: Record<string, unknown>) => {
+      await api.post('api/images', data);
+    },
     {
       // TODO ONSUCCESS MUTATION
+      onSuccess: () => {
+        // https://react-query.tanstack.com/reference/QueryClient#queryclientinvalidatequeries
+        queryClient.invalidateQueries();
+      },
     }
   );
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState,
-    setError,
-    trigger,
-  } = useForm();
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
   const { errors } = formState;
 
   const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
     try {
       // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+      if (!imageUrl) {
+        toast({
+          title: 'Imagem não adicionada',
+          status: 'info',
+          description:
+            'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro',
+        });
+      }
       // TODO EXECUTE ASYNC MUTATION
+      // https://react-query.tanstack.com/guides/mutations
+      await mutation.mutateAsync({
+        title: data.title as string,
+        description: data.description as string,
+        url: imageUrl,
+      });
       // TODO SHOW SUCCESS TOAST
+      toast({
+        title: 'Imagem cadastrada',
+        status: 'success',
+        description: 'Sua imagem foi cadastrada com sucesso.',
+      });
     } catch {
       // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        title: 'Falha no cadastro',
+        status: 'error',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.',
+      });
     } finally {
       // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      setImageUrl('');
+      setLocalImageUrl('');
+      closeModal();
     }
   };
 
@@ -67,20 +119,29 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           setLocalImageUrl={setLocalImageUrl}
           setError={setError}
           trigger={trigger}
+          name="image"
           // TODO SEND IMAGE ERRORS
+          error={errors.image}
           // TODO REGISTER IMAGE INPUT WITH VALIDATIONS
+          {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
+          name="title"
           // TODO SEND TITLE ERRORS
+          error={errors.title}
           // TODO REGISTER TITLE INPUT WITH VALIDATIONS
+          {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
+          name="description"
           // TODO SEND DESCRIPTION ERRORS
+          error={errors.description}
           // TODO REGISTER DESCRIPTION INPUT WITH VALIDATIONS
+          {...register('description', formValidations.description)}
         />
       </Stack>
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,34 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type FetchImageParams = {
+  after?: {
+    id: string;
+  };
+  data: {
+    title: string;
+    description: string;
+    url: string;
+    ts: number;
+    id: string;
+  }[];
+};
+
 export default function Home(): JSX.Element {
+
+  const fetchImages = async ({
+    pageParam = 0,
+  }): Promise<FetchImageParams> => {
+    const response = await api.get('api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return response.data;
+  };
+
+  // Infinite Queries
+  // https://react-query.tanstack.com/guides/infinite-queries
   const {
     data,
     isLoading,
@@ -16,28 +43,53 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastRequest => {
+      if (lastRequest.after) {
+        return lastRequest.after;
+      }
+      return null;
+    },
+  });
 
+  // ------------------------------
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    if (data) {
+      return data.pages.map(page => page.data).flat();
+    }
+    return null;
   }, [data]);
 
+
   // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
   // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
-      <Header />
-
+      <Header />      
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {
+          /* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */
+          hasNextPage && (
+            <Button
+              isLoading={isFetchingNextPage}
+              loadingText="Carregando..."
+              spinnerPlacement="start"
+              onClick={() => fetchNextPage()}
+            >
+              Carregar mais
+            </Button>
+          )
+        }
       </Box>
     </>
   );
