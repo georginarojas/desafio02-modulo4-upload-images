63af9bad58ff0dd62542e231910971ef8a9663b4
georginarojas <ginarojas29@gmail.com>
2021-08-08 13:41:48 -0300

    made the viewImage

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -2,11 +2,12 @@ import {
   Modal,
   ModalOverlay,
   ModalContent,
-  ModalFooter,
   ModalBody,
   Image,
   Link,
+  ModalFooter,
 } from '@chakra-ui/react';
+import React from 'react';
 
 interface ModalViewImageProps {
   isOpen: boolean;
@@ -19,5 +20,25 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+    <Modal motionPreset="slideInBottom" onClose={onClose} isOpen={isOpen}>
+      <ModalOverlay />
+      <ModalContent maxHeight="600px" maxWidth="900px">
+        <ModalBody padding={0} bgColor="pGray.800">
+          <Image src={imgUrl} maxH="600px" w="100%" objectFit="contain" />
+        </ModalBody>
+        <ModalFooter
+          bg="gray.800"
+          justifyContent="flex-start"
+          borderBottomLeftRadius="6px"
+          borderBottomRightRadius="6px"
+        >
+          <Link href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
